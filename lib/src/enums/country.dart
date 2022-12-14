f0119e47ee321387dd526ce48bea3b61a23457e7
Moaz El-sawaf <moazelsawaf@hotmail.com>
2022-12-14 14:48:00 +0200

    docs: :memo: add api docs for all countries and languages

--- lib/src/enums/country.dart
+++ lib/src/enums/country.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: constant_identifier_names, public_member_api_docs
+// ignore_for_file: constant_identifier_names
 
 /// The list of all supported countries
 ///
@@ -9,264 +9,781 @@
 ///
 /// If you have the `Country Code` as `String` and you want to get the corresponding [Country] enum member, then you can use [fromCode] method.
 enum Country {
+  /// Andorra
   ad,
+
+  /// United Arab Emirates
   ae,
+
+  /// Afghanistan
   af,
+
+  /// Antigua and Barbuda
   ag,
+
+  /// Anguilla
   ai,
+
+  /// Albania
   al,
+
+  /// Armenia
   am,
+
+  /// Angola
   ao,
+
+  /// Antarctica
   aq,
+
+  /// Argentina
   ar,
+
+  /// American Samoa
   c_as,
+
+  /// Austria
   at,
+
+  /// Australia
   au,
+
+  /// Aruba
   aw,
+
+  /// Aland Islands
   ax,
+
+  /// Azerbaijan
   az,
+
+  /// Bosnia and Herzegovina
   ba,
+
+  /// Barbados
   bb,
+
+  /// Bangladesh
   bd,
+
+  /// Belgium
   be,
+
+  /// Burkina Faso
   bf,
+
+  /// Bulgaria
   bg,
+
+  /// Bahrain
   bh,
+
+  /// Burundi
   bi,
+
+  /// Benin
   bj,
+
+  /// Saint Barthelemy
   bl,
+
+  /// Bermuda
   bm,
+
+  /// Brunei
   bn,
+
+  /// Bolivia
   bo,
+
+  /// Bonaire, Saint Eustatius and Saba
   bq,
+
+  /// Brazil
   br,
+
+  /// Bahamas
   bs,
+
+  /// Bhutan
   bt,
+
+  /// Bouvet Island
   bv,
+
+  /// Botswana
   bw,
+
+  /// Belarus
   by,
+
+  /// Belize
   bz,
+
+  /// Canada
   ca,
+
+  /// Cocos Islands
   cc,
+
+  /// Democratic Republic of the Congo
   cd,
+
+  /// Central African Republic
   cf,
+
+  /// Republic of the Congo
   cg,
+
+  /// Switzerland
   ch,
+
+  /// Ivory Coast
   ci,
+
+  /// Cook Islands
   ck,
+
+  /// Chile
   cl,
+
+  /// Cameroon
   cm,
+
+  /// China
   cn,
+
+  /// Colombia
   co,
+
+  /// Clipperton Island
   cp,
+
+  /// Costa Rica
   cr,
+
+  /// Cuba
   cu,
+
+  /// Cape Verde
   cv,
+
+  /// Curacao
   cw,
+
+  /// Christmas Island
   cx,
+
+  /// Cyprus
   cy,
+
+  /// Czech Republic
   cz,
+
+  /// Germany
   de,
+
+  /// Diego Garcia
   dg,
+
+  /// Djibouti
   dj,
+
+  /// Denmark
   dk,
+
+  /// Dominica
   dm,
+
+  /// Dominican Republic
   c_do,
+
+  /// Algeria
   dz,
+
+  /// Ceuta and Melilla
   ea,
+
+  /// Ecuador
   ec,
+
+  /// Estonia
   ee,
+
+  /// Egypt
   eg,
+
+  /// Western Sahara
   eh,
+
+  /// Eritrea
   er,
+
+  /// Spain
   es,
+
+  /// Ethiopia
   et,
+
+  /// European Union
   eu,
+
+  /// Finland
   fi,
+
+  /// Fiji
   fj,
+
+  /// Falkland Islands
   fk,
+
+  /// Micronesia
   fm,
+
+  /// Faroe Islands
   fo,
+
+  /// France
   fr,
+
+  /// Gabon
   ga,
+
+  /// United Kingdom
   gb,
+
+  /// Grenada
   gd,
+
+  /// Georgia
   ge,
+
+  /// French Guiana
   gf,
+
+  /// Guernsey
   gg,
+
+  /// Ghana
   gh,
+
+  /// Gibraltar
   gi,
+
+  /// Greenland
   gl,
+
+  /// Gambia
   gm,
+
+  /// Guinea
   gn,
+
+  /// Guadeloupe
   gp,
+
+  /// Equatorial Guinea
   gq,
+
+  /// Greece
   gr,
+
+  /// South Georgia and the South Sandwich Islands
   gs,
+
+  /// Guatemala
   gt,
+
+  /// Guam
   gu,
+
+  /// Guinea-Bissau
   gw,
+
+  /// Guyana
   gy,
+
+  /// Hong Kong
   hk,
+
+  /// Heard Island and McDonald Islands
   hm,
+
+  /// Honduras
   hn,
+
+  /// Croatia
   hr,
+
+  /// Haiti
   ht,
+
+  /// Hungary
   hu,
+
+  /// Canary Islands
   ic,
+
+  /// Indonesia
   id,
+
+  /// Ireland
   ie,
+
+  /// Israel
   il,
+
+  /// Isle of Man
   im,
+
+  /// India
   c_in,
+
+  /// British Indian Ocean Territory
   io,
+
+  /// Iraq
   iq,
+
+  /// Iran
   ir,
+
+  /// Iceland
   c_is,
+
+  /// Italy
   it,
+
+  /// Jersey
   je,
+
+  /// Jamaica
   jm,
+
+  /// Jordan
   jo,
+
+  /// Japan
   jp,
+
+  /// Kenya
   ke,
+
+  /// Kyrgyzstan
   kg,
+
+  /// Cambodia
   kh,
+
+  /// Kiribati
   ki,
+
+  /// Comoros
   km,
+
+  /// Saint Kitts and Nevis
   kn,
+
+  /// North Korea
   kp,
+
+  /// South Korea
   kr,
+
+  /// Kuwait
   kw,
+
+  /// Cayman Islands
   ky,
+
+  /// Kazakhstan
   kz,
+
+  /// Laos
   la,
+
+  /// Lebanon
   lb,
+
+  /// Saint Lucia
   lc,
+
+  /// Liechtenstein
   li,
+
+  /// Sri Lanka
   lk,
+
+  /// Liberia
   lr,
+
+  /// Lesotho
   ls,
+
+  /// Lithuania
   lt,
+
+  /// Luxembourg
   lu,
+
+  /// Latvia
   lv,
+
+  /// Libya
   ly,
+
+  /// Morocco
   ma,
+
+  /// Monaco
   mc,
+
+  /// Moldova
   md,
+
+  /// Montenegro
   me,
+
+  /// Saint Martin
   mf,
+
+  /// Madagascar
   mg,
+
+  /// Marshall Islands
   mh,
+
+  /// Macedonia
   mk,
+
+  /// Mali
   ml,
+
+  /// Myanmar
   mm,
+
+  /// Mongolia
   mn,
+
+  /// Macao
   mo,
+
+  /// Northern Mariana Islands
   mp,
+
+  /// Martinique
   mq,
+
+  /// Mauritania
   mr,
+
+  /// Montserrat
   ms,
+
+  /// Malta
   mt,
+
+  /// Mauritius
   mu,
+
+  /// Maldives
   mv,
+
+  /// Malawi
   mw,
+
+  /// Mexico
   mx,
+
+  /// Malaysia
   my,
+
+  /// Mozambique
   mz,
+
+  /// Namibia
   na,
+
+  /// New Caledonia
   nc,
+
+  /// Niger
   ne,
+
+  /// Norfolk Island
   nf,
+
+  /// Nigeria
   ng,
+
+  /// Nicaragua
   ni,
+
+  /// Netherlands
   nl,
+
+  /// Norway
   no,
+
+  /// Nepal
   np,
+
+  /// Nauru
   nr,
+
+  /// Niue
   nu,
+
+  /// New Zealand
   nz,
+
+  /// Oman
   om,
+
+  /// Panama
   pa,
+
+  /// Peru
   pe,
+
+  /// French Polynesia
   pf,
+
+  /// Papua New Guinea
   pg,
+
+  /// Philippines
   ph,
+
+  /// Pakistan
   pk,
+
+  /// Poland
   pl,
+
+  /// Saint Pierre and Miquelon
   pm,
+
+  /// Pitcairn
   pn,
+
+  /// Puerto Rico
   pr,
+
+  /// Palestine
   ps,
+
+  /// Portugal
   pt,
+
+  /// Palau
   pw,
+
+  /// Paraguay
   py,
+
+  /// Qatar
   qa,
+
+  /// Reunion
   re,
+
+  /// Romania
   ro,
+
+  /// Serbia
   rs,
+
+  /// Russia
   ru,
+
+  /// Rwanda
   rw,
+
+  /// Saudi Arabia
   sa,
+
+  /// Solomon Islands
   sb,
+
+  /// Seychelles
   sc,
+
+  /// Sudan
   sd,
+
+  /// Sweden
   se,
+
+  /// Singapore
   sg,
+
+  /// Ascension Island
   sh_ac,
+
+  /// Tristan da Cunha
   sh_ta,
+
+  /// Saint Helena
   sh,
+
+  /// Slovenia
   si,
+
+  /// Svalbard and Jan Mayen
   sj,
+
+  /// Slovakia
   sk,
+
+  /// Sierra Leone
   sl,
+
+  /// San Marino
   sm,
+
+  /// Senegal
   sn,
+
+  /// Somalia
   so,
+
+  /// Suriname
   sr,
+
+  /// South Sudan
   ss,
+
+  /// Sao Tome and Principe
   st,
+
+  /// El Salvador
   sv,
+
+  /// Sint Maarten
   sx,
+
+  /// Syria
   sy,
+
+  /// Swaziland
   sz,
+
+  /// Turks and Caicos Islands
   tc,
+
+  /// Chad
   td,
+
+  /// French Southern Territories
   tf,
+
+  /// Togo
   tg,
+
+  /// Thailand
   th,
+
+  /// Tajikistan
   tj,
+
+  /// Tokelau
   tk,
+
+  /// East Timor
   tl,
+
+  /// Turkmenistan
   tm,
+
+  /// Tunisia
   tn,
+
+  /// Tonga
   to,
+
+  /// Turkey
   tr,
+
+  /// Trinidad and Tobago
   tt,
+
+  /// Tuvalu
   tv,
+
+  /// Taiwan
   tw,
+
+  /// Tanzania
   tz,
+
+  /// Ukraine
   ua,
+
+  /// Uganda
   ug,
+
+  /// United States Minor Outlying Islands
   um,
+
+  /// United Nations
   united_nations,
+
+  /// United States
   us,
+
+  /// Uruguay
   uy,
+
+  /// Uzbekistan
   uz,
+
+  /// Vatican
   va,
+
+  /// Saint Vincent and the Grenadines
   vc,
+
+  /// Venezuela
   ve,
+
+  /// British Virgin Islands
   vg,
+
+  /// U.S. Virgin Islands
   vi,
+
+  /// Vietnam
   vn,
+
+  /// Vanuatu
   vu,
+
+  /// Wallis and Futuna
   wf,
+
+  /// Samoa
   ws,
+
+  /// Kosovo
   xk,
+
+  /// Unknown Country
   xx,
+
+  /// Yemen
   ye,
+
+  /// Mayotte
   yt,
+
+  /// South Africa
   za,
+
+  /// Zambia
   zm,
+
+  /// Zimbabwe
   zw;
 
   /// Returns the [Country] enum member from the given [countryCode] String.
--- lib/src/enums/language.dart
+++ lib/src/enums/language.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: constant_identifier_names, public_member_api_docs
+// ignore_for_file: constant_identifier_names
 
 /// The list of all supported languages.
 ///
@@ -10,154 +10,451 @@
 ///
 /// If you have the `Language Code` as `String` and you want to get the corresponding [Language] enum member, then you can use [fromCode] method.
 enum Language {
+  /// Afrikaans
   af,
+
+  /// Akan
   ak,
+
+  /// Amharic
   am,
+
+  /// Arabic (Iraq)
   ar_iq,
+
+  /// Arabic
   ar,
+
+  /// Assamese
   l_as,
+
+  /// Aymara
   ay,
+
+  /// Azerbaijani
   az,
+
+  /// Belarusian
   be,
+
+  /// Bulgarian
   bg,
+
+  /// Bhojpuri
   bho,
+
+  /// Bislama
   bi,
+
+  /// Bambara
   bm,
+
+  /// Bengali
   bn,
+
+  /// Bosnian
   bs,
+
+  /// Catalan
   ca,
+
+  /// Cebuano
   ceb,
+
+  /// Chamorro
   ch,
+
+  /// Mari
   chm,
+
+  /// Corsican
   co,
+
+  /// Czech
   cs,
+
+  /// Welsh
   cy,
+
+  /// Danish
   da,
+
+  /// German
   de,
+
+  /// Divehi
   dv,
+
+  /// Dzongkha
   dz,
+
+  /// Greek
   el,
+
+  /// English (Australia)
   en_au,
+
+  /// English (Canada)
   en_ca,
+
+  /// English (India)
   en_in,
+
+  /// English (Nigeria)
   en_ng,
+
+  /// English (New Zealand)
   en_nz,
+
+  /// English (United States)
   en_us,
+
+  /// English (South Africa)
   en_za,
+
+  /// English
   en,
+
+  /// Spanish
   es,
+
+  /// Estonian
   et,
+
+  /// Basque
   eu,
+
+  /// Persian
   fa,
+
+  /// Finnish
   fi,
+
+  /// Filipino
   fil,
+
+  /// Fijian
   fj,
+
+  /// Faroese
   fo,
+
+  /// French
   fr,
+
+  /// Irish
   ga,
+
+  /// Galician
   gl,
+
+  /// Guarani
   gn,
+
+  /// Gujarati
   gu,
+
+  /// Manx
   gv,
+
+  /// Hausa
   ha,
+
+  /// Hawaiian
   haw,
+
+  /// Hebrew
   he,
+
+  /// Hindi
   hi,
+
+  /// Hiri Motu
   ho,
+
+  /// Croatian
   hr,
+
+  /// Haitian
   ht,
+
+  /// Hungarian
   hu,
+
+  /// Armenian
   hy,
+
+  /// Indonesian
   id,
+
+  /// Igbo
   ig,
+
+  /// Iloko
   ilo,
+
+  /// Icelandic
   l_is,
+
+  /// Italian
   it,
+
+  /// Japanese
   ja,
+
+  /// Javanese
   jv,
+
+  /// Georgian
   ka,
+
+  /// Kazakh
   kk,
+
+  /// Kalaallisut
   kl,
+
+  /// Central Khmer
   km,
+
+  /// Kannada
   kn,
+
+  /// Korean
   ko,
+
+  /// Krio
   kri,
+
+  /// Kurdish
   ku,
+
+  /// Kirghiz
   ky,
+
+  /// Latin
   la,
+
+  /// Luxembourgish
   lb,
+
+  /// Ganda
   lg,
+
+  /// Lingala
   ln,
+
+  /// Lao
   lo,
+
+  /// Lithuanian
   lt,
+
+  /// Luba-Katanga
   lu,
+
+  /// Latvian
   lv,
+
+  /// Malagasy
   mg,
+
+  /// Marshallese
   mh,
+
+  /// Maori
   mi,
+
+  /// Macedonian
   mk,
+
+  /// Malayalam
   ml,
+
+  /// Mongolian
   mn,
+
+  /// Marathi
   mr,
+
+  /// Western Mari
   mrj,
+
+  /// Malay
   ms,
+
+  /// Maltese
   mt,
+
+  /// Burmese
   my,
+
+  /// Nauru
   na,
+
+  /// Norwegian Bokmål
   nb,
+
+  /// North Ndebele
   nd,
+
+  /// Nepali
   ne,
+
+  /// Dutch
   nl,
+
+  /// Norwegian Nynorsk
   nn,
+
+  /// Norwegian
   no,
+
+  /// South Ndebele
   nr,
+
+  /// Chichewa
   ny,
+
+  /// Punjabi
   pa,
+
+  /// Papiamento
   pap,
+
+  /// Polish
   pl,
+
+  /// Pashto
   ps,
+
+  /// Portuguese
   pt_br,
+
+  /// Portuguese
   pt,
+
+  /// Rundi
   rn,
+
+  /// Romanian
   ro,
+
+  /// Russian
   ru,
+
+  /// Kinyarwanda
   rw,
+
+  /// Sindhi
   sd,
+
+  /// Sango
   sg,
+
+  /// Sinhala
   si,
+
+  /// Slovak
   sk,
+
+  /// Slovenian
   sl,
+
+  /// Samoan
   sm,
+
+  /// Shona
   sn,
+
+  /// Somali
   so,
+
+  /// Albanian
   sq,
+
+  /// Serbian
   sr,
+
+  /// Swati
   ss,
+
+  /// Southern Sotho
   st,
+
+  /// Sundanese
   su,
+
+  /// Swedish
   sv,
+
+  /// Swahili
   sw,
+
+  /// Tamil
   ta,
+
+  /// Telugu
   te,
+
+  /// Tajik
   tg,
+
+  /// Thai
   th,
+
+  /// Turkmen
   tk,
+
+  /// Tagalog
   tl,
+
+  /// Tswana
   tn,
+
+  /// Tonga
   to,
+
+  /// Turkish
   tr,
+
+  /// Tahitian
   ty,
+
+  /// Ukrainian
   uk,
+
+  /// Urdu
   ur,
+
+  /// Uzbek
   uz,
+
+  /// Vietnamese
   vi,
+
+  /// Xhosa
   xh,
+
+  /// Unknown Language
   xx,
+
+  /// Yiddish
   yi,
+
+  /// Yoruba
   yo,
+
+  /// Yucateco
   yua,
+
+  /// Chinese - Traditional
   zh_TW,
+
+  /// Chinese
   zh,
+
+  /// Zulu
   zu;
 
   /// Returns the [Language] enum member from the given [languageCode] String.
