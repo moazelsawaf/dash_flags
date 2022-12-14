36ef06e299ef2ac99c827c714f9b466f312b52b0
Moaz El-sawaf <moazelsawaf@hotmail.com>
2022-12-14 03:03:05 +0200

    docs: add an example

---
+++ example/android/app/src/main/kotlin/dev/moaz/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.moaz.dash_flags_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,98 @@
+// Dart imports:
+import 'dart:async';
+
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:dash_flags/dash_flags.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dash Flags - Example',
+      theme: ThemeData(useMaterial3: true),
+      home: const FlagsScreen(),
+    );
+  }
+}
+
+class FlagsScreen extends StatefulWidget {
+  const FlagsScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<FlagsScreen> createState() => _FlagsScreenState();
+}
+
+class _FlagsScreenState extends State<FlagsScreen> {
+  final List<Country> _countries = Country.values;
+  final List<Language> _languages = Language.values;
+  int _selectedCountryIndex = 0;
+  int _selectedLanguageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        setState(() {
+          _selectedCountryIndex == _countries.length - 1
+              ? _selectedCountryIndex = 0
+              : _selectedCountryIndex++;
+
+          _selectedLanguageIndex == _languages.length - 1
+              ? _selectedLanguageIndex = 0
+              : _selectedLanguageIndex++;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dash Flags - Example'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Country: ${_countries[_selectedCountryIndex].name.toUpperCase()}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            CountryFlag(
+              country: _countries[_selectedCountryIndex],
+              height: 200,
+            ),
+            const SizedBox(height: 32),
+            Text(
+              'Language: ${_languages[_selectedLanguageIndex].name.toUpperCase()}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            LanguageFlag(
+              language: _languages[_selectedLanguageIndex],
+              height: 200,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
